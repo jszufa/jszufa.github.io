143ccafefb108bbdb7904096b3205b4f238e2ed1
jszufa <118739912+jszufa@users.noreply.github.com>
2023-01-17 16:54:17 +0100

    zadanie 3 ajax gotowe

---
+++ 3-pobierz-dane-ajax-jquery/js/main.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+
+    $('#get-data').click(function () {
+
+        $.get('https://akademia108.pl/api/ajax/get-post.php')
+
+            .done(function (data) {
+
+                console.log(data);
+                let userId = data.userId;
+                let id = data.id;
+                let title = data.title;
+                let body = data.body;
+
+                $('#get-data').after('<\hr>').after(`<p>Body : ${body}</p>`).after(`<p>Title : ${title}</p>`).after(`<p>ID : ${id}</p>`).after(`<p>User ID : ${userId} </p>`);
+            })
+
+            .fail(function (error) {
+                console.error(error);
+            })
+
+    })
+})
+
+
+
